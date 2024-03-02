a8f75660c76cde446f1ef86f40c2bbf8eea9d9d1
symelbak <suhaas2004y@gmail.com>
2024-03-02 17:09:40 +0530

    made changes to reg_id

--- Parkingdb.sql
+++ Parkingdb.sql
@@ -27,13 +27,13 @@ CREATE TABLE Token(
 
 INSERT INTO Admin VALUES(111,'admin');
 
-INSERT INTO Registration VALUES(1000,'John', 'Doe', 'john.doe@gmail.com', 9998882221, 'Staff', 'KA19 EQ 1316');
+INSERT INTO Registration VALUES(9000,'John', 'Doe', 'john.doe@gmail.com', 9998882221, 'Staff', 'KA19 EQ 1316');
 INSERT INTO Registration VALUES(1004, 'Emily', 'Chen', 'emily_chen@gmail.com', 8888222244, 'Student', 'KA19 P 8488');
 INSERT INTO Registration VALUES(1007, 'Mark', 'P', 'mark.p@gmail.com', 7788221144, 'Student', 'KA09 G 8888');
 
 INSERT INTO Token VALUES(1,'AB1', NULL, NULL, NULL);
 INSERT INTO Token VALUES(2,'AB2', NULL, NULL, NULL);
-INSERT INTO Token VALUES(3,'AB3', 1000, 'KA19 EQ 1316', '13:26:20');
+INSERT INTO Token VALUES(3,'AB3', 9000, 'KA19 EQ 1316', '13:26:20');
 INSERT INTO Token VALUES(4,'AB4', NULL, NULL, NULL);
 INSERT INTO Token VALUES(5,'AB5', 1007, 'KA09 G 8888', '09:11:48');
 INSERT INTO Token VALUES(6,'AB6', NULL, NULL, NULL);
